276a13e31947cd0dd667f2a764bacc9070704af6
Johanna Backlund Wass <johanna.bw1@gmail.com>
2022-11-28 16:09:28 +0100

    styling on ad improved, styling for filteroptions done

--- controllers/AdController.js
+++ controllers/AdController.js
@@ -3,7 +3,7 @@ import AdModel from "../models/AdModel.js";
 async function getAllAds(req, res) {
     const Ads = await AdModel.find();
     const locals = { Ads };
-    console.log(Ads);
+    // console.log(Ads);
     res.render("index", locals);
 }
 
