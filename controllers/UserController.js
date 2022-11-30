86a9ecb584e8756d032b2300174c3ab14d54533f
Johanna Backlund Wass <johanna.bw1@gmail.com>
2022-11-30 15:16:52 +0100

    register user now saves a user to the collection Users succesfully

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -27,43 +27,6 @@ async function registerUser(req, res) {
     }
 }
 
-async function logForm(req, res) {
-    console.log("Form submitted");
-    res.redirect("login");
-}
-
-// async function publishAd(req, res) {
-//     // let query = null;
-  
-//     try {
-//       // collect data from body
-//       const { title, category, price, description, visibility } = req.body;
-  
-//     //   const postedBy = ObjectId(req.session.userId);
-  
-//       // create Quote document instance locally
-//       const newAdDocument = new AdModel({ title, category, price, description, visibility });
-      
-//       // save to database
-//       newAdDocument.save();
-  
-//     // create message that operation was successfull
-//     //   query = new URLSearchParams({type: "success", message: "Successfully created quote!"});
-//     } catch (err) {
-//       // create message that operation was unsuccessfull
-//     //   query = new URLSearchParams({type: "fail", message: err.message});
-//       console.error(err.message);
-//     } finally {
-//     //   const queryStr = query.toString();
-//     //   res.redirect(`/quotes?${queryStr}`);
-//     res.redirect("/ads");
-//     }
-//   }
-
-//   async function getAdForm(req, res) {
-//     res.render("adForm");
-//   }
-
 // async function login(req, res) {
 //     try {
 //         // collect data from body
@@ -99,4 +62,4 @@ async function logForm(req, res) {
 // }
 
 
-export default { getLogin, getRegisterForm, registerUser, logForm }
+export default { getLogin, getRegisterForm, registerUser }
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -3,10 +3,11 @@ import UserController from "../controllers/UserController.js";
 
 const UserRouter = express.Router();
 
+// LOGIN
 UserRouter.get("/", UserController.getLogin);
-UserRouter.get("/register", UserController.getRegisterForm);
 
+// REGISTER
+UserRouter.get("/register", UserController.getRegisterForm);
 UserRouter.post("/register", UserController.registerUser);
-UserRouter.post("/", UserController.logForm);
 
 export default UserRouter;
