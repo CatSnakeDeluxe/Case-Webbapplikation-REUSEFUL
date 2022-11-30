8663861690e5323775895efbd8907c1057d82564
Johanna Backlund Wass <johanna.bw1@gmail.com>
2022-11-30 15:46:44 +0100

    bcrypt password works

--- controllers/AdController.js
+++ controllers/AdController.js
@@ -16,26 +16,26 @@ async function publishAd(req, res) {
   
     try {
       // collect data from body
-      const { title, category, price, description, visibility } = req.body;
-  
-    //   const postedBy = ObjectId(req.session.userId);
-  
-      // create Quote document instance locally
-      const newAdDocument = new AdModel({ title, category, price, description, visibility });
-      
-      // save to database
-      newAdDocument.save();
-  
+        const { title, category, price, description, visibility } = req.body;
+    
+        //   const postedBy = ObjectId(req.session.userId);
+    
+        // create Quote document instance locally
+        const newAdDocument = new AdModel({ title, category, price, description, visibility });
+        
+        // save to database
+        newAdDocument.save();
+    
     // create message that operation was successfull
     //   query = new URLSearchParams({type: "success", message: "Successfully created quote!"});
     } catch (err) {
       // create message that operation was unsuccessfull
     //   query = new URLSearchParams({type: "fail", message: err.message});
-      console.error(err.message);
+        console.error(err.message);
     } finally {
     //   const queryStr = query.toString();
     //   res.redirect(`/quotes?${queryStr}`);
-    res.redirect("/ads");
+        res.redirect("/ads");
     }
   }
 
