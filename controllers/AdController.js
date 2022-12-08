cb266980df8a1fe9e8abf673831c43a52963c069
Johanna Backlund Wass <johanna.bw1@gmail.com>
2022-12-08 13:23:21 +0100

    added nicer pictures based on categories

--- controllers/AdController.js
+++ controllers/AdController.js
@@ -4,11 +4,6 @@ import AdModel from "../models/AdModel.js";
 async function getAllAds(req, res) {
   const Ads = await AdModel.find({ visibility: "public" }).populate("postedBy", "username").exec();
   const userId = req.session.userId;
-  // Ads.forEach(ad => {
-  //   console.log("Ads", Ads.category);
-  // });
-  // functions.addImg(Ads.category);
- 
   const locals = { Ads, userId };
 
   return res.render("ads", locals);
