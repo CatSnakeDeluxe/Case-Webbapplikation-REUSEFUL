d275d93204c628857a0511511b8342668fa825ce
Johanna Backlund Wass <johanna.bw1@gmail.com>
2022-12-08 14:44:35 +0100

    changed ads layout on userPage and added images based on category

--- functions/functions.js
+++ functions/functions.js
@@ -37,10 +37,10 @@ function handleEdit(id, oldTitle, oldCategory, oldPrice, oldDescription, oldVisi
     };
 }
 
-// setTimeout(() => {
-// const msg = document.getElementById("serverMessage");
-// msg.style.display = "none";
-// }, 5000);
+setTimeout(() => {
+const msg = document.getElementById("serverMessage");
+msg.style.display = "none";
+}, 4000);
 
 function handleDelete(id) {
     console.log("Delete was triggered with id", id);
